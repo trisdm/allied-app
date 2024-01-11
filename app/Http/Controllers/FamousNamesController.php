835ddf94fec0cf2160b56c12e49940e36b0635a2
Your Name <you@example.com>
2024-01-11 14:39:44 +0000

    reading json data

--- app/Http/Controllers/FamousNamesController.php
+++ app/Http/Controllers/FamousNamesController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Illuminate\View\View;
 
 class FamousNamesController extends Controller
 {
     //
     public function index() : View {
-        return View('famousNames');
+
+        $file = File::get(base_path('storage/app/private/famous-names.json'));
+        $jsonData = json_decode($file, true);
+
+        return View('famousNames', ['names' => $jsonData]);
     }
 }
